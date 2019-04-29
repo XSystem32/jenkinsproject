028f1513b190b2e406af95e5655e190a79ba041e
XSystem32 <x_system16@hotmail.com>
2019-04-29 15:12:46 +0200

    tets of change

--- src/main/java/com/example/helloworld/HomeController.java
+++ src/main/java/com/example/helloworld/HomeController.java
@@ -3,8 +3,12 @@ package com.example.helloworld;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.ArrayList;
+
 @Controller
 public class HomeController {
+
+    ArrayList<String> names = new ArrayList<>();
 
     @GetMapping("/")
     public String home() {
